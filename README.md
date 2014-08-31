6f7c5e17fe329f089b474bcaff33cee5a48d20ef
Bjorn Hansson <bjorn@bjornhansson.se>
2014-08-31 12:54:11 +0200

    Cleanup and new favicon

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-arngrim
+Arngrim
 =======
 
-Lightweight bug and issue tracker
+A lightweight bug and issue tracker I made just during one weekend. To be used in my small projects.
--- index.php
+++ index.php
@@ -30,7 +30,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" href="img/favicon.ico">
+    <link rel="icon" href="img/favicon.png">
     <title><?php echo TITLE; ?></title>
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
